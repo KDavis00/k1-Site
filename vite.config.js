8ce4b7c926b0e621ac5bdcc35503ab6312fac520
KDavis00 <138532072+KDavis00@users.noreply.github.com>
2025-07-25 23:01:42 -0400

    udate config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/k1-Site/',  // Set the base path for your project
+  base: '/k1-Site/', // 👈 THIS IS CRUCIAL
   plugins: [react()],
-})
+});
